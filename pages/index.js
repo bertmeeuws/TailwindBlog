bdb561ef16cc2b3528334d8538b4ca2dce1762bb
Bert Meeuws <bert.meeuws@outlook.com>
2022-03-23 15:42:04 +0100

    Add list projects

--- pages/index.js
+++ pages/index.js
@@ -74,14 +74,12 @@ export default function Home({ posts }) {
                   <li className=" relative">
                     <article className="group relative  z-10 flex flex-col space-y-8 rounded-md border border-black bg-white p-4 transition-all duration-100 hover:bg-purplePrimary group-hover:text-white">
                       <p className="font-medium text-gray-500 group-hover:text-white">
-                        March 8, 2022
+                        <time dateTime={date}>{formatDate(date)}</time>
                       </p>
                       <div>
-                        <p className="mb-2 text-3xl font-bold group-hover:text-white">
-                          Sample .md file
-                        </p>
+                        <p className="mb-2 text-3xl font-bold group-hover:text-white">{title}</p>
                         <p className="font-medium text-gray-600 group-hover:text-white">
-                          Example of a markdown file with code blocks and syntax highlighting
+                          {summary}
                         </p>
                       </div>
                     </article>
@@ -91,69 +89,33 @@ export default function Home({ posts }) {
               )
             })}
         </ul>
-        <ul className="hidden divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
+      </div>
+      <div className="mt-16 mb-8 flex items-center justify-between">
+        <h2 className="text-xl font-bold">Some of my projects</h2>
+        <p className="cursor-pointer text-gray-600">View all articles</p>
+      </div>
+      <ul className="grid grid-cols-1 gap-14 px-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        {posts.map((i) => {
+          const { slug, date, title, summary, tags } = i
+          return (
+            <Link key={slug}>
+              <li className=" relative">
+                <article className="group relative  z-10 flex flex-col space-y-8 rounded-md border border-black bg-white p-4 transition-all duration-100 hover:bg-purplePrimary group-hover:text-white">
+                  <p className="font-medium text-gray-500 group-hover:text-white">
+                    <time dateTime={date}>{formatDate(date)}</time>
+                  </p>
+                  <div>
+                    <p className="mb-2 text-3xl font-bold group-hover:text-white">{title}</p>
+                    <p className="font-medium text-gray-600 group-hover:text-white">{summary}</p>
                   </div>
                 </article>
+                <div className="absolute left-4 top-4 h-full w-full rounded-md border-2 border-purplePrimary"></div>
               </li>
-            )
-          })}
-        </ul>
-      </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="all posts"
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-      )}
+            </Link>
+          )
+        })}
+      </ul>
+
       {siteMetadata.newsletter.provider !== '' && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
